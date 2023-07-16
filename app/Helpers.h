bf277239de49c9fe5543ca3cb284a45f4ec54337
FlynnGuo <93301049+FlynnYGUO@users.noreply.github.com>
2023-07-16 15:21:54 -0400

    delete bool plotVerbose

--- app/Helpers.h
+++ app/Helpers.h
@@ -9,7 +9,6 @@ bool verbose = false; // default to false, true for debugging, a lot of printout
 bool myfileVerbose = false;
 bool throwfileVerbose = false;
 bool hadronhitVerbose = false;
-bool plotVerbose = false;
 bool ntupleVerbose = false;
 
 unsigned long N_throws = 64*64; // Use multiple of 64
