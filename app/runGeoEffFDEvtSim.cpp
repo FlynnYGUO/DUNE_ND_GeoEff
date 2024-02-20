3a84d3e8b3d1dd036453678ab50ddff8049a1da9
FlynnGuo <93301049+FlynnYGUO@users.noreply.github.com>
2024-02-20 00:20:53 -0500

    Try to use the same lepPDG as in the ND

--- app/runGeoEffFDEvtSim.cpp
+++ app/runGeoEffFDEvtSim.cpp
@@ -89,6 +89,7 @@ int main(int argc, char** argv)
   int FD_Sim_nMu; // # of Sim muons (mu+/mu-)
   int FD_CCNC_truth; // 0 =CC 1 =NC
   int FD_neuPDG; // Generator level neutrino PDG
+  int FD_LepPDG; // Lepton PDG
   double FD_Sim_mu_start_vx; // Position of the muon trajectory at start point on the x-axis [cm]
   double FD_Sim_mu_start_vy; // Position of the muon trajectory at start point on the y-axis [cm]
   double FD_Sim_mu_start_vz; // Position of the muon trajectory at start point on the z-axis [cm]
@@ -121,6 +122,7 @@ int main(int argc, char** argv)
   t->SetBranchAddress("Sim_nMu",                  &FD_Sim_nMu);
   t->SetBranchAddress("CCNC_truth",               &FD_CCNC_truth);
   t->SetBranchAddress("neuPDG",                   &FD_neuPDG);
+  t->SetBranchAddress("LepPDG",                   &FD_LepPDG);
   t->SetBranchAddress("Sim_mu_start_vx",          &FD_Sim_mu_start_vx);
   t->SetBranchAddress("Sim_mu_start_vy",          &FD_Sim_mu_start_vy);
   t->SetBranchAddress("Sim_mu_start_vz",          &FD_Sim_mu_start_vz);
@@ -629,7 +631,8 @@ int main(int argc, char** argv)
     if (myfileVerbose) myfile << "FD_Sim_n_hadronic_Edep_b: " << FD_Sim_n_hadronic_Edep_b <<"\n";
     if (throwfileVerbose) myfile << "FD_Sim_n_hadronic_Edep_b: " << FD_Sim_n_hadronic_Edep_b <<"\n";
     if ( FD_CCNC_truth == 1) continue;   // only use CC events
-    if ( abs(FD_neuPDG) != 14 ) continue;       // only use muon neu
+    // if ( abs(FD_neuPDG) != 14 ) continue;       // only use muon neu
+    if ( abs(FD_LepPDG) != 13 ) continue;       // only use muon 
     // Only pick the events' vertex inside the FD FV
     if(FD_Sim_mu_start_vx > FD_FV_max[0] || FD_Sim_mu_start_vx < FD_FV_min[0] || FD_Sim_mu_start_vy > FD_FV_max[1] || FD_Sim_mu_start_vy < FD_FV_min[1] || FD_Sim_mu_start_vz > FD_FV_max[2] || FD_Sim_mu_start_vz < FD_FV_min[2]) continue;
     FD_FV_counter++;
